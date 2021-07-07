5c69c6cc4cf45f744e2b45dc59e96974cd22a22e
psingh038 <77434182+psingh038@users.noreply.github.com>
2021-07-07 19:33:32 +0530

    Update my-script.js

--- my-script.js
+++ my-script.js
@@ -1,5 +1,13 @@
+ var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
+jasmine.loadConfigFile('spec/support/jasmine.json');
+
+jasmine.execute();
+const utils = require("js/my-script.js");
+
 describe("Test Suite", function() {
     it("test spec", function() {
-        expect( expression ).toEqual(true);
+        expect( "loadUserData").toEqual(true);
     }); 
 });
